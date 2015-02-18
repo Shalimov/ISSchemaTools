be2c296e3ce1bfe25dacb3f3707c78e0374e1a1d
Igor Shalimov <ShaLimich@gmail.com>
2015-02-19 00:21:10 +0300

    Added tests for internal Underscore set

--- src/isschema-tools-core.js
+++ src/isschema-tools-core.js
@@ -1,30 +1,50 @@
-ISSchemaTools = (function() {
+var ISSchemaTools = (function () {
     'use strict';
+
+    var root = {};
+    var chainNs = {};
+    var objFormatPattern = /\$\{(\w+)\}/g;
+    var argsFormatPattern = /\$\{(\d+)\}/g;
 
     var _ = {
         _toString: Object.prototype.toString,
 
-        isNaN: function(val) {
+        _formatObj: function (str, data) {
+            return str.replace(objFormatPattern, function (gr, capture) {
+                return data[capture] || '';
+            });
+        },
+
+        _formatArgs: function () {
+            var str = arguments[0];
+            var args = Array.prototype.slice.call(arguments, 1);
+
+            return str.replace(argsFormatPattern, function (gr, capture) {
+                return args[capture] || '';
+            });
+        },
+
+        isNaN: function (val) {
             return val !== val;
         },
 
-        isFunction: function(func) {
-            return func && this._toString.call(func) === '[object Function]';
+        isFunction: function (func) {
+            return !!func && this._toString.call(func) === '[object Function]';
         },
 
-        isObject: function(obj) {
+        isObject: function (obj) {
             return obj !== null && typeof obj === 'object';
         },
 
-        isNumber: function(n) {
+        isNumber: function (n) {
             return this._toString.call(n) === '[object Number]' && !this.isNaN(n) && isFinite(n);
         },
 
-        isBoolean: function(b) {
+        isBoolean: function (b) {
             return typeof b === 'boolean';
         },
 
-        isEmpty: function(val, globalAllow, localAllow) {
+        isEmpty: function (val, globalAllow, localAllow) {
             var isNull = val === null;
 
             if (isNull && _.isBoolean(localAllow)) {
@@ -33,14 +53,14 @@ ISSchemaTools = (function() {
                 isNull = !globalAllow;
             }
 
-            return isNull || val === undefined || val === "" || this.isNaN(val);
+            return isNull || val === undefined || val === "" || this.isNaN(val) || val === Infinity;
         },
 
-        isExpectedTypeOrNull: function(val, type) {
+        isExpectedTypeOrNull: function (val, type) {
             return val === null || val.constructor === type;
         },
 
-        each: function(instance, fn, ctx) {
+        each: function (instance, fn, ctx) {
             var i, length, keys, key;
 
             if (Array.isArray(instance)) {
@@ -57,11 +77,11 @@ ISSchemaTools = (function() {
             }
         },
 
-        compact: function(array) {
+        compact: function (array) {
             if (!Array.isArray(array)) return array;
 
             var result = [];
-            this.each(array, function(val) {
+            this.each(array, function (val) {
                 if (!_.isEmpty(val)) {
                     result.push(val);
                 }
@@ -70,12 +90,34 @@ ISSchemaTools = (function() {
             return result;
         },
 
-        extend: function(obj, extendObj) {
+        extend: function (obj, extendObj) {
             if (!(this.isObject(obj) || this.isObject(extendObj))) return;
 
-            this.each(extendObj, function(val, key) {
+            this.each(extendObj, function (val, key) {
                 this[key] = val;
             }, obj);
+        },
+
+        format: function () {
+            var args = arguments;
+            var argsCount = args.length;
+            var str = args[0];
+            var data = args[1];
+
+            if (argsCount === 2 && Array.isArray(data)) {
+                var argumentsArray = data;
+                argumentsArray.unshift(str);
+
+                return this._formatArgs.apply(this, argumentsArray);
+            } else if (argsCount === 2 && this.isObject(data)) {
+
+                return this._formatObj(str, data);
+            } else if (argsCount > 1) {
+
+                return this._formatArgs.apply(this, args);
+            }
+
+            return str;
         }
     };
 
@@ -84,21 +126,15 @@ ISSchemaTools = (function() {
         _.extend(this, init);
     }
 
-    function ObjNode(key, value, level, path, parent, pattern) {
-        this.key = key;
-        this.value = value;
-        this.level = level;
-        this.path = path || [];
-        this.parent = parent;
-        this.pattern = pattern;
-    }
-
-    function createNode(val, key, path, level, parent, pattern) {
-        return new ObjNode(key, val, level, path, parent, pattern);
-    }
-
-    function rule(init) {
-        return new Rule(init);
+    function createNode(value, key, path, level, parent, pattern) {
+        return {
+            key: key,
+            value: value,
+            level: level,
+            path: path || [],
+            parent: parent,
+            pattern: pattern
+        };
     }
 
     function traverse(obj, fn) {
@@ -159,71 +195,129 @@ ISSchemaTools = (function() {
         return nodes;
     }
 
-    function clean(nodes, options) {
-        var target;
-        var root = {};
-        var compactCache = [];
-
+    function clean(options) {
         options = options || {};
 
-        var allowNull = options.allowNull;
+        var allowNull = options.allowNull || false;
 
-        nodes.forEach(function(node) {
+        this._nodes = this._nodes.filter(function (node) {
             var value = node.value;
             var pattern = node.pattern;
+            return !_.isEmpty(value, allowNull, pattern.allowNull) && _.isExpectedTypeOrNull(value, pattern.type);
+        });
 
-            if (!_.isEmpty(value, allowNull, pattern.allowNull) && _.isExpectedTypeOrNull(value, pattern.type)) {
+        return this;
+    }
 
-                var pathes = node.path;
-                target = root;
+    function build() {
+        var isRootArray = this._isRootArray;
+        var root = isRootArray ? [] : {};
+        var compactCache = [];
+        var target;
 
-                for (var i = 0, length = pathes.length; i < length; i += 1) {
-                    var path = pathes[i];
-                    var nextPath = pathes[i + 1];
+        this._nodes.forEach(function (node) {
+            var value = node.value;
+            var pathes = node.path;
+            target = root;
 
-                    if (target[path] === undefined) {
-                        if (_.isNumber(nextPath)) {
-                            compactCache.push({
-                                target: target,
-                                targetKey: path
-                            });
+            for (var i = 0, length = pathes.length; i < length; i += 1) {
+                var path = pathes[i];
+                var nextPath = pathes[i + 1];
 
-                            target[path] = [];
-                        } else if (nextPath !== undefined) {
-                            target[path] = {};
-                        } else {
-                            target[path] = value;
-                        }
+                if (target[path] === undefined) {
+                    if (_.isNumber(nextPath)) {
+                        compactCache.push({
+                            target: target,
+                            targetKey: path
+                        });
+
+                        target[path] = [];
+                    } else if (nextPath !== undefined) {
+                        target[path] = {};
+                    } else {
+                        target[path] = value;
                     }
-
-                    target = target[path];
                 }
+
+                target = target[path];
             }
         });
 
-        compactCache.forEach(function(item) {
+        compactCache.forEach(function (item) {
             var t = item.target;
             var tk = item.targetKey;
             t[tk] = _.compact(t[tk]);
         });
 
-        return root;
+        return isRootArray ? _.compact(root) : root;
     }
 
-    return {
-        rule: rule,
+    function addToChain(methodName, method) {
+        if (chainNs.hasOwnProperty(moduleName)) {
+            throw new Error(_.format('Method [${name}] already exist in chain', {
+                name: methodName
+            }));
+        }
+
+        if (!_.isFunction(method)) {
+            throw new Error('Second argument must be a Function');
+        }
+
+        chainNs[methodName] = method.bind(chainNs);
+    }
+
+    function defineExtension(moduleName, providerFn, replaceIfExist) {
+        if (this.hasOwnProperty(moduleName) && !replaceIfExist) {
+            throw new Error(_.format('Module [${name}] already exist', {
+                name: moduleName
+            }));
+        }
+
+        if (!_.isFunction(providerFn)) {
+            throw new Error('Provider must be a Function');
+        }
+
+        var extension = providerFn.call(this, _, addToChain);
+
+        if (!_.isObject(extension)) {
+            throw new Error('Provider function must return an Object');
+        }
+
+        this[moduleName] = extension;
+    }
+
+    _.extend(chainNs, {
+        _isRootArray: null,
+        _nodes: null,
+        clean: (clean).bind(chainNs),
+        build: (build).bind(chainNs)
+    });
+
+    _.extend(root, {
         matchTraverse: matchTraverse,
         traverse: traverse,
-        clean: function(obj, pattern, options) {
-            var nodes = matchTraverse(obj, pattern);
-            return clean(nodes, options);
-        },
-        chain: function(obj, pattern) {
-            var nodes = matchTraverse(obj, pattern);
+        defineExtension: defineExtension,
 
-            return {
-                clean: (clean).bind(null, nodes)
-            };
+        rule: function (init) {
+            return new Rule(init);
+        },
+
+        chain: function (obj, pattern) {
+            chainNs._isRootArray = Array.isArray(obj);
+            chainNs._nodes = matchTraverse(obj, pattern);
+            return chainNs;
+        },
+
+        clean: function (obj, pattern, options) {
+            return this.chain(obj, pattern).clean(options).build();
         }
-    };
+    });
+
+    return root;
 })();
+
+if (module && module.exports) {
+    module.exports = ISSchemaTools;
+} else {
+    this.ISSchemaTools = ISSchemaTools;
+}
---
+++ tests/core.test.js
@@ -0,0 +1,298 @@
+var t = require('../src/isschema-tools-core');
+
+describe('test core methods of ISSchemaTools', function () {
+
+    describe('test small set of underscore/lodash functions', function () {
+        var _;
+
+        before(function () {
+            t.defineExtension('test', function (u) {
+                _ = u;
+                return _;
+            });
+        });
+
+        it('test defineExtension of function', function () {
+            t.should.have.property('test');
+        });
+
+        it('test existence of small set of functions of underscore/lodash', function () {
+            _.should.have.keys(['_toString', '_formatArgs', '_formatObj', 'isNaN', 'isFunction', 'isObject', 'isNumber', 'isBoolean', 'isEmpty', 'isExpectedTypeOrNull', 'each', 'compact', 'extend', 'format']);
+
+            _.should.matchEach(function (it) {
+                it.should.be.a.Function;
+            });
+        });
+
+        it('test _#isNaN', function () {
+            _.isNaN(Function).should.be.false;
+            _.isNaN("string").should.be.false;
+            _.isNaN("NaN").should.be.false;
+            _.isNaN(1).should.be.false;
+            _.isNaN({}).should.be.false;
+            _.isNaN([]).should.be.false;
+            _.isNaN(Infinity).should.be.false;
+            _.isNaN(false).should.be.false;
+            _.isNaN(true).should.be.false;
+            _.isNaN(undefined).should.be.false;
+            _.isNaN(null).should.be.false;
+            _.isNaN(NaN).should.be.true;
+        });
+
+        it('test _#isFunction', function () {
+            _.isFunction("string").should.be.false;
+            _.isFunction("NaN").should.be.false;
+            _.isFunction(1).should.be.false;
+            _.isFunction({}).should.be.false;
+            _.isFunction([]).should.be.false;
+            _.isFunction(Infinity).should.be.false;
+            _.isFunction(false).should.be.false;
+            _.isFunction(undefined).should.be.false;
+            _.isFunction(null).should.be.false;
+            _.isFunction(true).should.be.false;
+            _.isFunction(NaN).should.be.false;
+            _.isFunction(Function).should.be.true;
+        });
+
+        it('test _#isObject', function () {
+            _.isObject("string").should.be.false;
+            _.isObject("NaN").should.be.false;
+            _.isObject(1).should.be.false;
+            _.isObject(Infinity).should.be.false;
+            _.isObject(false).should.be.false;
+            _.isObject(true).should.be.false;
+            _.isObject(NaN).should.be.false;
+            _.isObject(Function).should.be.false;
+            _.isObject(undefined).should.be.false;
+            _.isObject(null).should.be.false;
+            _.isObject([]).should.be.true;
+            _.isObject({}).should.be.true;
+        });
+
+        it('test _#isNumber', function () {
+            _.isNumber("string").should.be.false;
+            _.isNumber("NaN").should.be.false;
+            _.isNumber(Infinity).should.be.false;
+            _.isNumber(false).should.be.false;
+            _.isNumber(true).should.be.false;
+            _.isNumber(NaN).should.be.false;
+            _.isNumber(Function).should.be.false;
+            _.isNumber(undefined).should.be.false;
+            _.isNumber(null).should.be.false;
+            _.isNumber([]).should.be.false;
+            _.isNumber({}).should.be.false;
+            _.isNumber(1).should.be.true;
+        });
+
+        it('test _#isBoolean', function () {
+            _.isBoolean("string").should.be.false;
+            _.isBoolean("NaN").should.be.false;
+            _.isBoolean(Infinity).should.be.false;
+            _.isBoolean(NaN).should.be.false;
+            _.isBoolean(Function).should.be.false;
+            _.isBoolean(undefined).should.be.false;
+            _.isBoolean(null).should.be.false;
+            _.isBoolean([]).should.be.false;
+            _.isBoolean({}).should.be.false;
+            _.isBoolean(1).should.be.false;
+            _.isBoolean(true).should.be.true;
+            _.isBoolean(false).should.be.true;
+        });
+
+        it('test _#isEmpty', function () {
+            _.isEmpty([]).should.be.false;
+            _.isEmpty({}).should.be.false;
+            _.isEmpty(Function).should.be.false;
+            _.isEmpty("string").should.be.false;
+            _.isEmpty(true).should.be.false;
+            _.isEmpty(false).should.be.false;
+
+            _.isEmpty("").should.be.true;
+            _.isEmpty(undefined).should.be.true;
+            _.isEmpty(Infinity).should.be.true;
+            _.isEmpty(NaN).should.be.true;
+
+            _.isEmpty(null).should.be.true;
+            _.isEmpty(null, false).should.be.true;
+            _.isEmpty(null, true, false).should.be.true;
+            _.isEmpty(null, false, true).should.be.false;
+            _.isEmpty(null, true, false).should.be.true;
+            _.isEmpty(null, true).should.be.false;
+        });
+
+        it('test _#isExpectedTypeOrNull', function () {
+            _.isExpectedTypeOrNull("string", String).should.be.true;
+            _.isExpectedTypeOrNull(0, Number).should.be.true;
+            _.isExpectedTypeOrNull(Function, Function).should.be.true;
+            _.isExpectedTypeOrNull([], Array).should.be.true;
+            _.isExpectedTypeOrNull({}, Object).should.be.true;
+            _.isExpectedTypeOrNull(true, Boolean).should.be.true;
+            _.isExpectedTypeOrNull(false, Boolean).should.be.true;
+            _.isExpectedTypeOrNull(null, String).should.be.true;
+
+            _.isExpectedTypeOrNull(false, String).should.be.false;
+            _.isExpectedTypeOrNull([], Function).should.be.false;
+        });
+
+        it('test _#each', function () {
+            var values = ['apple', 'banana', 'dinos'];
+            var count = values.length;
+
+            _.each(values, function (val, i) {
+                count--;
+                values[i].should.be.eql(val);
+            });
+
+            count.should.be.eql(0);
+
+            var obj = {key1: 'prop', 'key2': 'prop', key3: 'prop'};
+
+            count = Object.keys(obj).length;
+            _.each(obj, function (val, key) {
+                count--;
+                obj[key].should.be.eql(val);
+            });
+
+            count.should.be.eql(0);
+        });
+
+        it('test _#compact', function () {
+            var values = [null, 1, 0, NaN, Infinity, false, true, "", "string", {}, [], undefined, Function];
+
+            var result = _.compact(values);
+
+            result.should.have.length(8);
+
+            result.should.matchEach(function (it) {
+                _.isEmpty(it).should.be.false;
+            });
+
+            result.should.be.eql([1, 0, false, true, "string", {}, [], Function]);
+        });
+
+        it('test _#extend', function () {
+            var testObj = {prop0: 0};
+
+            _.extend(testObj, {
+                prop1: '1',
+                prop2: '2'
+            });
+
+            testObj.should.be.eql({prop0: 0, prop1: '1', prop2: '2'});
+
+            _.extend(testObj, {
+                prop2: 2,
+                prop3: '3'
+            });
+
+            testObj.should.be.eql({prop0: 0, prop1: '1', prop2: 2, prop3: '3'});
+        });
+
+        it('test _#format', function () {
+            _.format('str ${here}', {here: 1}).should.be.eql('str 1');
+            _.format('str ${here}').should.be.eql('str ${here}');
+            _.format('str ${here}', {here1: 1}).should.be.eql('str ');
+            _.format('str ${0}').should.be.eql('str ${0}');
+            _.format('str ${0}', 1).should.be.eql('str 1');
+            _.format('str ${0}-${1}', 1, 2).should.be.eql('str 1-2');
+            _.format('str ${0}-${1}', [1, 2, 3]).should.be.eql('str 1-2');
+            _.format('str ${0}', []).should.be.eql('str ');
+
+        });
+
+        after(function () {
+            delete t.test;
+        });
+
+    });
+
+    describe('test #clean functionality', function () {
+
+        it('test clean functionality with Simple Root Object', function () {
+            var simpleModel = {
+                name: 'name',
+                surname: 'surname',
+                index: 1,
+                hidden: false
+            };
+
+            t.chain(simpleModel, {
+                name: t.rule({
+                    type: String
+                }),
+                surname: t.rule({
+                    type: String
+                }),
+                index: t.rule({
+                    type: Number
+                })
+            }).clean().build().should.have.keys(['name', 'surname', 'index']);
+        });
+
+        it('test #clean functionality with Array as Root Object and non object types', function () {
+            var simpleModel = ['val0', {value: 2}, {}, 1, 2, null, {value: 1}, 'val1'];
+
+            t.clean(simpleModel, [t.rule({type: String})]).should.have.keys(['0', '1']);
+            t.chain(simpleModel, [t.rule({type: String})]).clean().build().should.have.keys(['0', '1']);
+        });
+
+        it('test #clean functionality with Array as Root Object and object types', function () {
+            var simpleModel = ['val0', {value: 2}, {}, 1, 2, null, {value: 1}];
+
+            t.clean(simpleModel, [{value: t.rule({type: Number})}]).should.have.keys(['0', '1']);
+            t.chain(simpleModel, [{value: t.rule({type: Number})}]).clean().build().should.have.keys(['0', '1']);
+        });
+
+        it('test #clean functionality with Object as Root and Complex data in nodes', function () {
+            var complexModel = {
+                data: {
+                    data: {
+                        data: [{
+                            value: [{
+                                data: 1
+                            }, {
+                                data: 2
+                            }, {
+                                abracadbra: 2
+                            }]
+                        }, {
+                            value: [{
+                                data: 4
+                            }]
+                        }, {
+                            jalouse: []
+                        }]
+                    }
+                }
+            };
+
+            t.clean(complexModel, {
+                data: {
+                    data: {
+                        data: [{
+                            value: [{
+                                data: t.rule({type: Number})
+                            }]
+                        }]
+                    }
+                }
+            }).should.have.eql({
+                    data: {
+                        data: {
+                            data: [{
+                                value: [{
+                                    data: 1
+                                }, {
+                                    data: 2
+                                }]
+                            }, {
+                                value: [{
+                                    data: 4
+                                }]
+                            }]
+                        }
+                    }
+                });
+        });
+    });
+});
